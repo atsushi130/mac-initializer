5914a0a9e3b3bed4bf7c52cf56d88b86fbf3d81f
atsushi130 <atsushi_main@outlook.jp>
2018-02-09 09:12:44 +0900

    first commit :tada:

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo xcodebuild -license
+
+# brew install
+/usr/bin/ruby -e '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)'
+
+brew install tmux
+brew install reattach-to-user-namespace
+sudo gem install bundler
+sudo gem install cocoapods -n /usr/local/bin
+brew install fzf
+brew cask install ngrok
+brew install pt
