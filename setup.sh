93e3ee5bf20f508e069b4a680b30793c15af0a51
atsushi130 <atsushi_main@outlook.jp>
2018-02-14 10:05:21 +0900

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ brew install tmux
 brew install reattach-to-user-namespace
 sudo gem install bundler
 sudo gem install cocoapods -n /usr/local/bin
+brew install carthage
 brew install fzf
 brew cask install ngrok
 brew install pt
